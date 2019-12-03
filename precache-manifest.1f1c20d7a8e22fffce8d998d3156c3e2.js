efca184a4494ba1f61c1515290cdacb4f0c35203
SergiyMi <bladeneo@i.ua>
2019-12-03 12:53:54 +0200

    Updates

---
+++ precache-manifest.1f1c20d7a8e22fffce8d998d3156c3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1911ade485b8194feb88aa753a6bb8d6",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "20485c1619585c3932b3",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "53562b00afbbc1178386",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.fc01244a.chunk.css"
+  },
+  {
+    "revision": "20485c1619585c3932b3",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.a2861335.chunk.js"
+  },
+  {
+    "revision": "53562b00afbbc1178386",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.78fc68fc.chunk.js"
+  },
+  {
+    "revision": "5847a7037a28a0a45d1a",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.41a5d37c.js"
+  }
+]);
